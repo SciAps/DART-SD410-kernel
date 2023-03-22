8c598b045db33bd828efb174231f8068898b84f9
Andre Doudkin <adoudkin@gmail.com>
2023-03-22 17:59:01 -0400

    Ketek DPP3 Support

---
+++ drivers/sciaps/sciaps-driver.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2022 SciAps
+ *
+ *
+ *
+ *
+ *
+ */
+
+#ifndef __SCIAPS_DRIVER_H__
+#define __SCIAPS_DRIVER_H__
+
+#define SCIAPS_DRIVER_ID_KETEK_DPP3_SPI		15
+
+#define SCIAPS_SPI_SPEED_HZ_Default			1000000
+
+#endif //__SCIAPS_DRIVER_H__
